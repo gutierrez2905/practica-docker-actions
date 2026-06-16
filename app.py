68052d3995fbdb7c091727d91ff29ce2aeef6726
gutierrez2905 <gutierrezmacias05@gmail.com>
2026-06-16 10:23:29 -0600

    Demostracion final 2

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ def sumar(a, b):
 
 if __name__ == "__main__":
     print("Ejecución del código...")
-    print(f"La suma de 5 y 3 es: {sumar(5, 3)} - DEMOSTRACION DE CAMBIO EN EL CODIGO")
+    print(f"La suma de 5 y 3 es: {sumar(5, 3)} - DEMO2")
